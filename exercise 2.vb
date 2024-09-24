657894ece6ea2f54612209d83d322bdae74f3245
Bafirawala Ireneous <bafiireneous@gmail.com>
2024-09-24 11:54:42 +0300

    Add files via upload

---
+++ exercise 2.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number As Integer
+
+        number = TextBox1.Text
+
+
+        If number < 0 Then
+            MessageBox.Show("Negative number")
+        ElseIf number = 0 Then
+            MessageBox.Show("Zero")
+
+        Else
+            MessageBox.Show("Positve number ")
+
+
+        End If
+
+
+    End Sub
+End Class
