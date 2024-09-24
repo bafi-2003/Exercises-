5b46f7fcf0c97974fb61895a79215c646ceeca98
Bafirawala Ireneous <bafiireneous@gmail.com>
2024-09-24 12:00:20 +0300

    Add files via upload

---
+++ exercise 3.1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i As Integer = 1 To 10
+            MessageBox.Show("Number:" & i)
+
+        Next
+
+        Dim number As Integer = Integer.Parse(TextBox1.Text)
+
+        While number >= 0
+            MessageBox.Show("number :" & number)
+            number -= 1
+        End While
+
+
+
+
+
+    End Sub
+
+End Class
